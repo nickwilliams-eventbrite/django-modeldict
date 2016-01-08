642b6f8c072296a1ee0e3ac4c11af309ca93f851
Adam Chainz <adam@adamj.eu>
2016-01-08 18:11:15 +0000

    Improved documentation
    * README has requirements and installation isntructions, better formatting for example usage
    * HISTORY file
    * README + HISTORY as long_description for PyPI display

---
+++ HISTORY.rst
@@ -0,0 +1,15 @@
+.. :changelog:
+
+=======
+History
+=======
+
+Pending Release
+---------------
+
+* Forked by YPlan
+
+1.4.1 (2012-12-04)
+------------------
+
+* Last release by Disqus
--- setup.py
+++ setup.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
 
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read().replace('.. :changelog:', '')
+
 setup(
     name='django-modeldict-yplan',
     version='1.4.2',
@@ -10,6 +16,7 @@ setup(
     maintainer_email='adam@yplanapp.com',
     url='http://github.com/YPlan/django-modeldict',
     description='Stores a model as a dictionary',
+    long_description=readme + '\n\n' + history,
     packages=find_packages(exclude=['tests']),
     zip_safe=False,
     include_package_data=True,
