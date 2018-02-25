4b7ce885d47260bc16abf39475409643a2efb4d7
Adam Johnson <me@adamj.eu>
2018-02-24 20:10:14 -0500

    Add python_requires to setup.py

--- setup.py
+++ setup.py
@@ -34,6 +34,7 @@ setup(
     zip_safe=False,
     license='Apache License 2.0',
     include_package_data=True,
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Framework :: Django',
